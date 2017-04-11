e56cc89178dc265dfa3d23910b154d985a4ce5d9
dcoollx <dsqjr@msn.com>
2017-04-11 19:02:07 -0400

    Update main.js
    this is a working version but does not take into account settings

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
 	var header = "http-equiv";
 	var enforced = false;
 	var tags = document.getElementsByTagName("meta");
+	if(tags.length<=0){
 	for(var x = 0;x>tags.length;x++ )
 	{
 		if(tags[x].getAttribute("http-equiv") == "Content Security Policy")
@@ -13,11 +14,15 @@
 			break;
 		}
 	}
+	}else{ console.log("no metas found");
 	if(!enforced)
 	{
 		console.log("no CSP found adding to page based on preference");
-		var meta = document.createElement("META");
-		meta.attributes
+		var meta = document.createElement("META");//todo create tag based on prefrences
+		meta.setAttribute("http-equiv", "Content-Security-Policy");
+		meta.setAttribute("content",CSP );
+		var head = document.getElementsByTagName("head")[0];
+		head.appendChild(meta);
 	}
 	
 }
